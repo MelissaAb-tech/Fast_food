e11154f1c62bb349c1b596800d4466529f27c0e6
Elsa <165050589+elsaContreras@users.noreply.github.com>
2025-01-05 01:12:17 +0100

    Update MenuVegetarienTest.java

--- FastFood/src/test/java/menu/MenuVegetarienTest.java
+++ FastFood/src/test/java/menu/MenuVegetarienTest.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package menu;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
@@ -13,23 +10,14 @@ import org.junit.jupiter.api.Test;
 public class MenuVegetarienTest extends MenuFactoryTest{
     @Override
     protected MenuFactory getMenuFactory() {
-        return new MenuVegetarienFactory();  // Renvoie l'instance de MenuClassiqueFactory
+        return new MenuVegetarienFactory();
     }
     @Test
     public void testMenuClassiqueSpecific() {
-        // Test spécifique à MenuClassiqueFactory (par exemple, vérifier les produits)
         Menu menu = getMenuFactory().getMenu();
-
-        // Vérifiez le nom du menu
         assertEquals("Menu végétarien", menu.getName());
-
-        // Vérifiez le prix du menu
         assertEquals(11.0, menu.getPrice(), 0.01);
-
-        // Vérifiez qu'il y a trois produits (un hamburger, une boisson, un dessert)
         assertEquals(3, menu.getProducts().size());
-
-        // Vérifiez que les produits sont de types corrects
         assertTrue(menu.getProducts().get(0) instanceof product.burger.Burger);
         assertTrue(menu.getProducts().get(1) instanceof product.drink.Drink);
         assertTrue(menu.getProducts().get(2) instanceof product.dessert.Dessert);
