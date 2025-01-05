6bc61f77897f6190d9e6d397d42b4e0b457413da
Elsa <165050589+elsaContreras@users.noreply.github.com>
2025-01-05 01:11:02 +0100

    Update MenuFactoryTest.java

--- FastFood/src/test/java/menu/MenuFactoryTest.java
+++ FastFood/src/test/java/menu/MenuFactoryTest.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package menu;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,19 +14,10 @@ public abstract class MenuFactoryTest {
 
     @Test
     public void testGetMenu() {
-        // Créer une instance de MenuFactory
         MenuFactory menuFactory = getMenuFactory();
-        
-        // Obtenez le menu généré par la factory
         Menu menu = menuFactory.getMenu();
-        
-        // Vérifiez que le nom du menu est correctement défini
         assertNotNull("Le nom du menu ne doit pas être nul", menu.getName());
-        
-        // Vérifiez que le prix du menu est bien défini
         assertTrue(menu.getPrice() > 0);
-        
-        // Vérifiez que le menu contient au moins un produit
         assertTrue( menu.getProducts().size() > 0);
     }
 }
